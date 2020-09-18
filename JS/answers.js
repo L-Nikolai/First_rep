9d096fc0354c03778a7d1ed40b5fd7b0ffb477ef
Nikolai <unsense.wow@gmail.com>
2020-09-18 17:21:30 +0300

    create 'answers' jscode

---
+++ JS/answers.js
@@ -0,0 +1,22 @@
+
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const firstQuestion = prompt('Один из просмотренных фильмов?',''),
+      secoonQuestion = prompt('На сколько оцените его?',''),
+      thrdQuestion = prompt('Один из просмотренных фильмов?',''),
+      fourthQuestion =  prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[firstQuestion] = secoonQuestion;
+personalMovieDB.movies[thrdQuestion] =fourthQuestion;
+
+console.log(personalMovieDB);
